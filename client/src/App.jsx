c0c316cb95b2361bea6462e21b7cc731e624a353
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-05-15 22:16:38 +0100

    set up an index.js in pages that exports all the pages. This way we can import it into App.jsx in one line via named exports. Created a basic router with four of the pages, including root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
+
 const router = createBrowserRouter([
-  { path: '/', element: <h1>home</h1> },
-  { path: '/about', element: <h1>about page</h1> },
+  { path: '/', element: <HomeLayout /> },
+  { path: '/register', element: <Register /> },
+  { path: '/login', element: <Login /> },
+  { path: '/dashboard', element: <DashboardLayout /> },
 ]);
 
 function App() {
